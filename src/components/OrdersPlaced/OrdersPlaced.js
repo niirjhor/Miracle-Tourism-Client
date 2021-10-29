a9ab5dea2473e478141fc587c41217947d90bc45
niirjhor <niirjhor@gmail.com>
2021-10-29 08:48:14 +0600

    Specific Package Done

--- src/components/OrdersPlaced/OrdersPlaced.js
+++ src/components/OrdersPlaced/OrdersPlaced.js
@@ -4,26 +4,21 @@ import { useParams } from 'react-router';
 const OrdersPlaced = () => {
     const { orderplacedId } = useParams();
     const [bookings, setBookings] = useState({});
-    const [specificBookingsDetails, setSpecificBookingsDetails] = useState({});
+    // const [specificBookingsDetails, setSpecificBookingsDetails] = useState({});
     useEffect(() => {
         fetch(`http://localhost:5000/packages/${orderplacedId}`)
             .then(res => res.json())
-            .then(data => setBookings(data.specificBookingsDetails))
+            .then(data => setBookings(data))
 
     }, [])
-    // useEffect(() => {
-    //     const matchedData = bookings.find(
-    //         (specificBookingsDetails) => specificBookingsDetails.orderplacedId === orderplacedId
-    //     );
-    //     setSpecificBookingsDetails(matchedData);
-    // }, [])
+
 
 
 
 
     return (
         <div>
-            <h2>Details of {bookings.name}</h2>
+            <h2>Details of {bookings.tourName}</h2>
             <h2>this is booking: {orderplacedId}</h2>
         </div>
     );
