8ea48c08fc0ed243e08cbffa10023055c453b34e
niirjhor <niirjhor@gmail.com>
2021-10-31 23:33:52 +0600

    Final

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            Not Found
+            <div className='container not-found'>
+                <h2>Error : 404</h2>
+                <h2 className='text-danger pb-5 mb-5'>Please Reload or Try again later with correct address</h2>
+            </div>
         </div>
     );
 };
